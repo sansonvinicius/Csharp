c8318f1ef8ced75a532b6eb5077ad1cb98759429
Vinicius Henrique Sanson <sansonvinicius@gmail.com>
2018-07-31 22:06:42 -0300

    Criando métodos de calcular

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -8,10 +8,10 @@ namespace Aula3107_Console
 {
     class Program
     {
-        private static int id;
-      
-
-        public static int Id { get => id; set => id = value; }
+        public static int Id { get; set; }
+        public static int num2 { get; set; }
+        public static int resp { get; set; }
+        public static String operação { get; set; }
        
 
         static void Main(string[] args)
@@ -20,9 +20,63 @@ namespace Aula3107_Console
 
             Console.WriteLine("Digite um nº");
             Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro nº");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a operação");
+            operação = Console.ReadLine();
 
-            Console.WriteLine("Você digitou:" + Id);
+            switch (operação)
+            {
+                case "+":
+                    SomarNumeros(num2, Id);
+                    break;
+                case "-":
+                    DiminuirNumeros(num2, Id);
+                    break;
+                case "/":
+                    DividirNumeros(num2, Id);
+                    break;
+                case "*":
+                    MultiplicarNumeros(num2, Id);
+                    break;
+            }
+            //Console.WriteLine("A soma dos nºs é " + soma);
+            //ImprimirNumeroDigitado(Id);
             Console.ReadKey();
         }
+
+        static void ImprimirNumeroDigitado (int num)
+        {
+             Console.WriteLine("Você digitou:" + num);
+        }
+
+        static void SomarNumeros(int num2, int Id)
+        {
+            resp = num2 + Id;
+            Console.WriteLine("A soma dos números é:" + resp);
+            
+        }
+
+        static void DiminuirNumeros(int num2, int Id)
+        {
+            resp = Id - num2;
+            Console.WriteLine("A subtração dos números é:" + resp);
+
+        }
+
+        static void DividirNumeros(int num2, int Id)
+        {
+            resp = Id/num2;
+            Console.WriteLine("A divisão dos números é:" + resp);
+
+        }
+
+        static void MultiplicarNumeros(int num2, int Id)
+        {
+            resp = Id * num2;
+            Console.WriteLine("A multiplicação dos números é:" + resp);
+
+        }
+
     }
 }
