f36706e26e51f99a1d5fec651635e40cb395e161
Vinicius Henrique Sanson <sansonvinicius@gmail.com>
2018-07-31 21:24:18 -0300

    Código Digite um nº

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -8,9 +8,20 @@ namespace Aula3107_Console
 {
     class Program
     {
+        private static int id;
+      
+
+        public static int Id { get => id; set => id = value; }
+       
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            Console.WriteLine("Digite um nº");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou:" + Id);
             Console.ReadKey();
         }
     }
