00b423298cf4e549c18267d995a45ca8541fc153
Vinicius Henrique Sanson <sansonvinicius@gmail.com>
2018-08-02 20:37:46 -0300

    Criando as classes carro, moto e veículo

---
+++ Aulas/Aula0208_POO/Modelos/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0208_POO.Modelos
+{
+    class Veiculo
+    {
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Marca { get; set; }
+    }
+}
