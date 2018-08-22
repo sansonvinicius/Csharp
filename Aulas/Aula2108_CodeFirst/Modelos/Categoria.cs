953caee637302d907f794ba0d3c2a45d53c9d168
sansonvinicius <sansonvinicius@gmail.com>
2018-08-21 21:30:18 -0300

    Criando Classe Categoria

---
+++ Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2108_CodeFirst.Modelos
+{
+    class Categoria
+    {
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
