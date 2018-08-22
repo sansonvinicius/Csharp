8eb5e19ae4d54a83d041262e9d6e9360b7b25018
sansonvinicius <sansonvinicius@gmail.com>
2018-08-21 22:01:32 -0300

    Criando construtor para indicar o nome da String de conexão

--- Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    public class Contexto:DbContext
     {
+        //ctor ->tab+tab : criando construtor
+        public Contexto():base("nomeStringConexao")
+        {
+
+        }
 
     }
 }
